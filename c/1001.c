6ed3cce5f8f7e63bad6403e0259a966c14621dae
ediclesio <ediclesiomaia@gmail.com>
2020-04-01 11:19:06 -0300

    add first code in c

---
+++ c/1001.c
@@ -0,0 +1,18 @@
+
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+#include <stdio.h>
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, x;
+
+    scanf("%d %d", &a, &b);
+
+    x = a + b;
+
+    printf("X = %d\n", x);
+ 
+    return 0;
+}
